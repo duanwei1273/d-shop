1924b2b1d626a33ebdb2471606e3a61416e6ae2f
林深时见鹿 <10073450+drduian@user.noreply.gitee.com>
2022-04-12 16:36:19 +0800

    fastclick配置完成

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+//引入fasteclick
+import FastClick from 'fastclick'
+if ('addEventListener' in document) {
+    document.addEventListener('DOMContentLoaded', function() {
+        FastClick.attach(document.body);
+    }, false);
+}
 
 
 const app = createApp(App)
